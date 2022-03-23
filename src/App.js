50306884285e3f54aa0badb9dcc2126ea0be062c
Justin Herpin <justindherpin@yahoo.com>
2022-03-22 21:54:44 -0700

    end of day 3/22/22: initial setup of app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './views/Dashboard';
+import Login from './views/Login';
+import Register from './views/Register'
 
 function App() {
 
 	return (
 		<>
-			<div>
-				<h1>My App</h1>
-			</div>
+			<Router>
+
+				<div>
+
+					<Routes>
+						<Route path='/' element={<Dashboard />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/register' element={<Register />} />
+					</Routes>
+
+				</div>
+
+			</Router>
 		</>
 	);
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+
+    <header>
+
+    </header>
+    
+  )
+}
+
+export default Header
