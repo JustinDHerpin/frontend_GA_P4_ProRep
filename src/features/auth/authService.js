d43c2fd1cefa52e51d3690316fb45c1a71288fe3
Justin Herpin <justindherpin@yahoo.com>
2022-04-01 10:46:15 -0700

    cors fixed - deployed ver seeded - updating login to correct uri

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -32,7 +32,7 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + "login", userData);
+  const response = await axios.post(API_URL + "api/v1/users/login", userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
